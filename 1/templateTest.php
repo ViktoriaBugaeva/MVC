981adc686edfc478dc370e1f666d8000e0186694
LaineJakutkina <38099059+LaineJakutkina@users.noreply.github.com>
2019-02-11 20:11:22 +0200

    4 update

---
+++ 1/templateTest.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+    ?>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+<?php
+$pageTitle = 'Пример шаблона';
+$array = array('one', 'two', 'three');
+require 'template.php';
+
